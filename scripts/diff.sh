dd831df5e5c8b565a4b8c4c57dd942a109820f2b
carla <kirkcohenc@gmail.com>
2020-12-10 12:25:21 +0200

    multi: add github workflow to sync lnd and lit docs folders

---
+++ scripts/diff.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+if [ $# -ne 3 ];
+    then echo "args required: source repo, source dir, target dir"
+    exit 1
+fi
+
+# We will get our files from the source repo/dir.
+sourceDir="$1"/"$2"
+
+# We will copy these files to destination dir.
+destDir="$3"
+
+# Create our destination dir in case it does not yet exist, because we cannot
+# diff a directory that does not exist.
+mkdir -p "$destDir"
+
+# Copy everything from source to destination, replacing what's there.
+cp -rf "$sourceDir"/* "$destDir"
+
+# Remove the source repo that we cloned.
+rm -rf "$1"
+
+# If our sync has resulted in any changes, set an output for the rest of our
+# workflow.
+if [ -n "$(git status --porcelain)" ];
+  then echo ::set-output name=have_diff::"true"
+fi
